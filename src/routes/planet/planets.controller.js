e836756e75234ab6e003a77cc3c867ff32c62618
experiencei <ayelojahighbee01@gmail.com>
2021-11-29 15:28:09 +0100

    starting with nasa api

--- src/routes/planet/planets.controller.js
+++ src/routes/planet/planets.controller.js
@@ -1,5 +1,10 @@
-const planets = [];
+const planets = require('../../models/planets.model')
+
 
 function getAllPlanets(req , res){
     return res.status(200).json(planets);
+}
+
+module.exports = {
+    getAllPlanets
 }
