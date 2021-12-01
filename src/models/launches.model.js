f6e4990949f940f3c46c8eafa7c7c4b58efd64b0
experiencei <ayelojahighbee01@gmail.com>
2021-12-01 16:46:32 +0100

    using mongoose db to populate our nasa api

--- src/models/launches.model.js
+++ src/models/launches.model.js
@@ -1,4 +1,5 @@
 const launchesDatabase = require('./launches.mongo')
+const planets = require('./planets.mongo')
 
 const launches = new Map();
 
@@ -34,6 +35,13 @@ function existsLaunchWithId(launchIdd){
 }
 
 async function saveLaunch(launch) {
+    const planet = await planets.findOne({
+        keplerName: launch.target,
+    })
+
+    if(!planet) {
+        throw new Error("No matching planet found")
+    }
     await launchesDatabase.updateOne({
         flightNumber : launch.flightNumber,
     } , launch , {
