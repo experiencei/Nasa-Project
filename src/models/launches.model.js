40ecae3a30b785d0bb1365acbf357ccb37dae50d
experiencei <ayelojahighbee01@gmail.com>
2021-12-06 20:26:03 +0100

    prepoluting and sorting before returning the result'

--- src/models/launches.model.js
+++ src/models/launches.model.js
@@ -124,6 +124,7 @@ async function getLatestFlightNumber(){
     return await launchesDatabase.find({} , { 
         "_id" : 0 , "__v" : 0,
     })
+    .sort({ flightNumber : 1})
     .skip(skip)
     .limit(limit)
 }
