1f8aaae055f5eab9537d386b3feb1e36cfd19d7f
experiencei <ayelojahighbee01@gmail.com>
2021-12-06 20:06:14 +0100

    working paginaiting our API

--- src/services/query.js
+++ src/services/query.js
@@ -1,4 +1,11 @@
 function getPagination(query) {
     const limit = Math.abs(query.limit);
+    const page = Math.abs(query.page);
+    const skip = (page - 1) * limit;
+
     
+    return {
+        skip ,
+        limit
+    }
 }
